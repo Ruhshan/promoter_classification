b9184310e6b0a614298609f53eed528bcb897a7e
Ruhshan <ruhshan.ahmed@gmail.com>
2018-10-02 23:44:22 +0600

    added base stacking

--- main.go
+++ main.go
@@ -4,22 +4,54 @@ import (
 	"biogo"
 	"fmt"
 	"strings"
+	"strconv"
 )
 
-func main() {
+func makegcskewdata(window int, step int, maxlen int){
 	parsed, _ := biogo.ParseFasta("ctv_total.txt")
-	gcskews, _ := biogo.CalculateGCSkew(parsed, 50, 5, 1000)
+	gcskews, _ := biogo.GCSkewSlidingWindow(parsed, window, step, maxlen)
+	count:= 0
+	for _, g := range gcskews {
+		results := strings.Join(g.Values, ";")
+		fmt.Println("ctv_"+strconv.Itoa(count)+";"+results + ";1")
+		count++
+	}
+
+	count = 0
+	parsed, _ = biogo.ParseFasta("si_total.txt")
+	gcskews, _ = biogo.GCSkewSlidingWindow(parsed, window, step, maxlen)
 
 	for _, g := range gcskews {
 		results := strings.Join(g.Values, ";")
-		fmt.Println(results + ";1")
+		fmt.Println("si_"+strconv.Itoa(count)+";"+results + ";0")
+		count++
 	}
+}
 
-	parsed2, _ := biogo.ParseFasta("si_total.txt")
-	gcskews2, _ := biogo.CalculateGCSkew(parsed2, 50, 5, 1000)
 
-	for _, g := range gcskews2 {
+func makebasestackingdata(window int, step int, maxlen int){
+	parsed, _ := biogo.ParseFasta("ctv_total.txt")
+	bs_engs, _ := biogo.BaseStackingSlidingWindow(parsed, window, step, maxlen)
+	count:= 0
+	for _, g := range bs_engs {
 		results := strings.Join(g.Values, ";")
-		fmt.Println(results + ";0")
+		fmt.Println("ctv_"+strconv.Itoa(count)+";"+results + ";1")
+		count++
 	}
+
+	count = 0
+	parsed, _ = biogo.ParseFasta("si_total.txt")
+	bs_engs, _ = biogo.BaseStackingSlidingWindow(parsed, window, step, maxlen)
+
+	for _, g := range bs_engs {
+		results := strings.Join(g.Values, ";")
+		fmt.Println("si_"+strconv.Itoa(count)+";"+results + ";0")
+		count++
+	}
+}
+func main() {
+	//makegcskewdata(100, 5, 1000)
+
+	makebasestackingdata(200,20,1000)
+
 }
